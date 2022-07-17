3127badd6e1c899b3b7ce52bf65b9f109c112b7d
shoyt55 <101358655+shoyt55@users.noreply.github.com>
2022-07-17 12:57:08 -0400

    blank

--- mouse.js
+++ mouse.js
@@ -2,7 +2,7 @@ autoType();
 
 function autoType() {
   const typedTwo = new Typed(".auto-type2", {
-      strings: ['Me', 'Moi', `أنا`,'Jeff', `Мне`,'मैं','我'], 
+      strings: ['Me', 'Mua','Moi', `أنا`,'Jeff', `Мне`,'मैं','我'], 
       typeSpeed: 100,
       backSpeed: 150,
       loop: true
